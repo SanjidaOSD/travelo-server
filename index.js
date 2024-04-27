7d0d783d7786c398c915c05b8bcd4137386d11ec
RIEAD <mdriead.bd@gmail.com>
2024-04-27 23:13:08 +0600

    Added get my list functionality

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
@@ -8,7 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cricab9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -39,10 +39,26 @@ async function run() {
         res.send(result)
     })
 
-    // Get all tourist data from database
+    // Get all tourists spot data from database
     app.get('/allTouristsSpot', async(req, res)=>{
         const cursor = spotCollection.find();
         const result = await cursor.toArray();
+        res.send(result)
+      }) 
+
+    // Get single spot details data from database
+    app.get('/allTouristsSpot/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query = {_id : new ObjectId(id)}
+        const result = await spotCollection.findOne(query);
+        res.send(result)
+      }) 
+
+    // Get single spot details data from database
+    app.get('/allTouristsSpot/email/:email', async(req, res)=>{
+        const id = req.params.email
+        const query = {email : id}
+        const result = await spotCollection.find(query).toArray();
         res.send(result)
       }) 
 
