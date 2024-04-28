5338e8c7b28e670fdf965f5e8e46c2079882bdb3
RIEAD <mdriead.bd@gmail.com>
2024-04-28 16:48:01 +0600

    country data get functionality added

--- index.js
+++ index.js
@@ -94,6 +94,12 @@ async function run() {
         res.send(result)
       })
 
+      app.get('/country', async(req, res)=>{
+        const cursor = countryCollection.find();
+        const result = await cursor.toArray();
+        res.send(result)
+      }) 
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
